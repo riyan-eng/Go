0b6c5e3ee91ca5ad04e194bfba9ea6a8cd3760a9
riyan-eng <fbriyant@gmail.com>
2022-04-18 08:17:15 +0700

    first commit

---
+++ 01/numbers.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("1+1 = ", 1+1)
+	fmt.Println("1-1 = ", 1-1)
+	fmt.Println("10*2 = ", 10*2)
+	fmt.Println("10/2 = ", 10/2)
+	fmt.Println("11%2 = ", 11%2)
+}
---
+++ 01/strings.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(len("Hello world"))
+	fmt.Println("Hello world"[1])
+	fmt.Println("Hello " + "world")
+}
